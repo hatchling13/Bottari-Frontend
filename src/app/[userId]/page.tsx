c94f7e62d58d4cba44451f4b292ab97c1d006478
Jeongwook Park <hatchling3713@gmail.com>
2023-09-13 14:53:40 +0900

    Matched page state with url

--- src/app/[userId]/page.tsx
+++ src/app/[userId]/page.tsx
@@ -1,5 +1,7 @@
+import { css } from '@/../styled-system/css';
 import { checkUserId, getSlugsOfBundles } from '../placeholder';
 import { notFound } from 'next/navigation';
+import Link from 'next/link';
 
 export default async function BundlesPage({
   params,
@@ -14,5 +16,25 @@ export default async function BundlesPage({
 
   const bundleSlugs = await getSlugsOfBundles(params.userId);
 
-  return <main></main>;
+  return (
+    <main>
+      <section className={css({ display: 'flex', gap: '5' })}>
+        {bundleSlugs.map((slug) => (
+          <Link
+            key={slug.id}
+            href={`${params.userId}/${slug.id}`}
+            className={css({
+              padding: '5',
+              border: '1px solid',
+              borderRadius: 'lg',
+              fontSize: '2xl',
+              fontWeight: 'bold',
+            })}
+          >
+            {slug.name}
+          </Link>
+        ))}
+      </section>
+    </main>
+  );
 }
